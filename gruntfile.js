b0f098963892adc6a6806d5090c67b2e82843653
Biggs Thorarensen <biggs@devillion.com>
2016-09-22 08:10:11 +0100

    grunt ejs_static setup

--- gruntfile.js
+++ gruntfile.js
@@ -10,8 +10,22 @@ module.exports = function(grunt) {
 	        keepalive: true
 	      }
 	    }
+	  },
+	  ejs_static: {
+	  	preview: {
+	  		options: {
+				dest: 'preview',
+				path_to_data: 'src/data.json',
+				path_to_layouts: 'src',
+				index_page: 'index',
+				parent_dirs: false,
+				underscores_to_dashes: true,
+				file_extension: '.html'
+			}
+	  	}
 	  }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-ejs-static');
 };
